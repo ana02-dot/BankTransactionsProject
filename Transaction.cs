c963962266480ac2b4446423c619b2187e8fe445
Ana Chkhutiashvili <ana.chkhutiashvili.1@btu.edu.ge>
2024-11-08 21:28:24 +0400

    uploaded

---
+++ Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Transaction
+    {
+        public DateTime Date { get; }
+        public double Amount { get; }
+        public string Type { get; }
+        public string Description { get; }
+
+        public Transaction(DateTime date, double amount, string type, string description)
+        {
+            Date = date;
+            Amount = amount;
+            Type = type;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date:yyyy-MM-dd HH:mm:ss} - {Type}: {Amount} - {Description}";
+        }
+    }
+}
